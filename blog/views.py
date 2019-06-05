5d4716313427fbff596b039a8ba3d81e2be0b017
fsajadi <s.sfatemeh@gmail.com>
2019-06-05 17:07:42 -0400

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def home (request):
-    return HttpResponse('<h1> My lovely Home </h1>')
-
-def about (request):
-    return HttpResponse('<h1> blog about </h1>')
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
